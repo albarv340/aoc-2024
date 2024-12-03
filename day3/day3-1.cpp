ee474858363172dba204e25d1f6b909f79a5700f
Albin Arvidsson <albar556@student.liu.se>
2024-12-03 07:18:40 +0100

    Day 3

---
+++ day3/day3-1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <string>
+
+int main()
+{
+    std::ifstream file("input.txt");
+
+    if (!file.is_open())
+    {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
+    }
+
+    std::string input((std::istreambuf_iterator<char>(file)),
+                      std::istreambuf_iterator<char>());
+
+    file.close();
+
+    std::regex pattern("mul\\((\\d+),(\\d+)\\)");
+
+    auto matches_begin = std::sregex_iterator(input.begin(), input.end(), pattern);
+    auto matches_end = std::sregex_iterator();
+
+    int sum{0};
+
+    for (std::sregex_iterator i = matches_begin; i != matches_end; ++i)
+    {
+        std::smatch match = *i;
+        if (match.size() > 1)
+        {
+            sum += std::stoi(match.str(1)) * std::stoi(match.str(2));
+        }
+    }
+
+    std::cout << sum << std::endl;
+
+    return 0;
+}
