019e951a40d5d01b7c70d6a5a0a414178f51d6ce
Albin Arvidsson <albar556@student.liu.se>
2024-12-10 06:47:29 +0100

    Day 10

---
+++ day10/day10-1.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <cmath>
+#include <set>
+
+int width;
+int height;
+
+std::set<std::pair<int, int>> get_score(std::vector<std::vector<int>> const &grid, int x, int y)
+{
+    std::set<std::pair<int, int>> ends;
+    if (grid[y][x] == 9)
+    {
+        return {{y, x}};
+    }
+    if (x > 0 && grid[y][x - 1] == grid[y][x] + 1)
+    {
+        auto other = get_score(grid, x - 1, y);
+        ends.insert(other.begin(), other.end());
+    }
+    if (y > 0 && grid[y - 1][x] == grid[y][x] + 1)
+    {
+        auto other = get_score(grid, x, y - 1);
+        ends.insert(other.begin(), other.end());
+    }
+    if (x + 1 < width && grid[y][x + 1] == grid[y][x] + 1)
+    {
+        auto other = get_score(grid, x + 1, y);
+        ends.insert(other.begin(), other.end());
+    }
+    if (y + 1 < height && grid[y + 1][x] == grid[y][x] + 1)
+    {
+        auto other = get_score(grid, x, y + 1);
+        ends.insert(other.begin(), other.end());
+    }
+
+    return ends;
+}
+
+int main()
+{
+    std::ifstream file("input.txt");
+
+    if (!file.is_open())
+    {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
+    }
+
+    std::vector<std::vector<int>> grid;
+    std::string line;
+    int row = 0;
+
+    while (getline(file, line))
+    {
+        std::vector<int> grid_row;
+        for (int col = 0; col < line.length(); col++)
+        {
+            int current_char = line[col] - '0';
+            grid_row.push_back(current_char);
+        }
+        grid.push_back(grid_row);
+        row++;
+    }
+
+    file.close();
+
+    width = grid[0].size();
+    height = row;
+
+    long long sum{0};
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            if (grid[y][x] != 0)
+                continue;
+            int score = get_score(grid, x, y).size();
+            sum += score;
+        }
+    }
+    std::cout << sum << std::endl;
+}
